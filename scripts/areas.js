a73fb0bab14925fee438fb5bb3ea8cfe133681d5
Morganero <morganero1@gmail.com>
2022-09-23 14:14:06 -0500

    finished up the areas clickevent

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,7 +1,8 @@
-import { getAreas, getGuests } from "./database.js"
+import { getAreas, getParkGuests } from "./database.js"
 
 const areas = getAreas()
-const guests = getGuests()
+const parkGuests = getParkGuests()
+
 
 export const areasList = () => {
     let html = '<ul class="area__list">'
@@ -14,47 +15,45 @@ export const areasList = () => {
 
     return html
 }
-//click event for area click displays current number of guests
-// iterate through areas to find area clicked on
-// when found iterate through guest ids of guests to match with guest id of areas and push it in array
-// use .length method to count guests and add them to string interpolated window alert
+// make listener to find number of people per area//
+// first identify clicked area 
 document.addEventListener("click", (clickEvent) => {
     const itemClicked = clickEvent.target
-    const numberOfGuestsInAreaArray = []
+    const numberOfGuestsArray = []
     if (itemClicked.id.startsWith("area")) {
+        //use split to separate string at -- into an array. deconstruct clicked item's array. assign second indexed variable to areaId 
         const [,areaId] = itemClicked.id.split("--")
-        //check to see if clicked item's id matches one of the area's id. once the right one is found, use it to find number of guestId's that match with list of guest.id's
+        //iterate through area of areas to find area.id that matches clicked parsedInt areaId and if it matches iterate through list of parkGuests to see if that parkGuests.parkId matches with area and push into numberOfGuestsArray to be counted later.
         for (const area of areas){
             if (area.id === parseInt(areaId)) {
-            // if more than one guest in an area, iterate through that array of guestId's, then iterate through array of guests to see if guest.id matches guestIdsOfArray
-                if (Array.isArray(area.guestId)){
-                    for (const guestIdsOfGuestIdArray of area.guestId){
-                        for(const guest of guests){
-                            if(guestIdsOfGuestIdArray === guest.id){
-                            numberOfGuestsInAreaArray.push(guestIdsOfGuestIdArray)
-                            }
+                    for (const parkGuest of parkGuests) {
+                        if (parkGuest.parkId === area.id) {
+                            numberOfGuestsArray.push(parkGuest)
                         }
-                    }
-                } else {
-                    for (const guest of guests) {
-                        if (guest.id === area.guestId) {
-                            numberOfGuestsInAreaArray.push(guest)
-                        }        
                     } 
+                //make if statement for window alert to display different output for single or multiple guests
+                if (numberOfGuestsArray.length < 2){
+                    window.alert(`There is ${numberOfGuestsArray.length} guest in this area`)
+                } else {
+                    window.alert(`There are ${numberOfGuestsArray.length} guests in this area`)
                 }
-            } 
-            if (numberOfGuestsInAreaArray.length === 1){
-                window.alert(`There is ${numberOfGuestsInAreaArray.length} guest in this area`)
-            } else if (numberOfGuestsInAreaArray.length === 0){
-                window.alert(`There are currently no guests in this area`)
-            } else {
-                window.alert(`There are ${numberOfGuestsInAreaArray.length} guests in this area`)
-            }            
+            }
+            
         }
 
     
     
-    }  
-}     
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
 )                                          
+
 
--- scripts/database.js
+++ scripts/database.js
@@ -29,6 +29,12 @@ const database = {
     },{
         id: 8,
         name: "Harvey Holder",
+    },{
+        id: 9,
+        name: "India Inez",
+    },{
+        id: 10,
+        name: "Jaylene Janson",
     }
     ],
 
@@ -68,41 +74,157 @@ const database = {
         id: 1,
         location:"NorthWest",
         title: "Lodge",
-        guestId: 1,
-        serviceId: [4, 5, 6, 7] 
+
     },{
         id: 2,
         location:"Northern",
         title: "Lost Wolf Hiking Trail",
-        guestId: 2,
-        serviceId: [7, 8, 9] 
+
     },{
         id: 3,
         location:"NorthEast",
         title: "Chamfort River",
-        guestId: 3,
-        serviceId: [1, 2, 3] 
+   
     },{
         id: 4,
         location:"SouthWest",
         title: "Gander River",
-        guestId: 4,
-        serviceId: [1, 8] 
+
     },{
         id: 5,
         location:"Southern",
         title: "Campgrounds",
-        guestId: 5,
-        serviceId: [6,4,5] 
+   
     },{
         id: 6,
         location:"Southeast",
         title: "Pine Bluffs Trails",
-        guestId: [7,8],
-        serviceId: [8,7,10] 
+  
     }
+    ],
+    parkGuests: [{
+        id: 1,
+        parkId: 1,
+        guestId: 8
+    },{
+        id: 2,
+        parkId: 2,
+        guestId: 7
+    },{
+        id: 3,
+        parkId: 2,
+        guestId: 6
+    },{
+        id: 4,
+        parkId: 2,
+        guestId: 5
+    },{
+        id: 5,
+        parkId: 3,
+        guestId: 4
+    },{
+        id: 6,
+        parkId: 4,
+        guestId: 3
+    },{
+        id: 7,
+        parkId: 5,
+        guestId: 2
+    },{
+        id: 8,
+        parkId: 6,
+        guestId: 1
+    },{
+        id: 8,
+        parkId: 5,
+        guestId: 9
+    },{
+        id: 8,
+        parkId: 4,
+        guestId: 10
+    }
+    ],
+    parkServices: [{
+        id: 1,
+        parkId: 1,
+        serviceId: 4
+    },{
+        id: 2,
+        parkId: 1,
+        serviceId: 5
+    },{
+        id: 3,
+        parkId: 1,
+        serviceId: 6
+    },{
+        id: 4,
+        parkId: 1,
+        serviceId: 7
+    },{
+        id: 5,
+        parkId: 2,
+        serviceId: 7
+    },{
+        id: 6,
+        parkId: 2,
+        serviceId: 8
+    },{
+        id: 7,
+        parkId: 2,
+        serviceId: 9
+    },{
+        id: 8,
+        parkId: 3,
+        serviceId: 1
+    },{
+        id: 9,
+        parkId: 3,
+        serviceId: 2
+    },{
+        id: 10,
+        parkId: 3,
+        serviceId: 3
+    },{
+        id: 11,
+        parkId: 4,
+        serviceId: 1
+    },{
+        id: 12,
+        parkId: 4,
+        serviceId: 8
+    },{
+        id: 13,
+        parkId: 5,
+        serviceId: 6
+    },{
+        id: 14,
+        parkId: 5,
+        serviceId: 4
+    },{
+        id: 15,
+        parkId: 5,
+        serviceId: 5
+    },{
+        id: 16,
+        parkId: 1,
+        serviceId: 8
+    },{
+        id: 17,
+        parkId: 6,
+        serviceId: 7
+    },{
+        id: 18,
+        parkId: 6,
+        serviceId: 8
+    },{
+        id: 19,
+        parkId: 6,
+        serviceId: 10
+    },
+
     ]
 }
+
 
 export const getGuests = () => {
     return database.guests.map(guest => ({...guest}))
@@ -112,5 +234,11 @@ export const getAreas = () => {
   }
 export const getServices = () => {
     return database.services.map(service => ({...service}))
+  }
+  export const getParkServices = () => {
+    return database.parkServices.map(parkServices => ({...parkService}))
+  }
+  export const getParkGuests = () => {
+    return database.parkGuests.map(parkGuest => ({...parkGuest}))
   }
 
